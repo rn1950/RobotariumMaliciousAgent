c905d4328d117c0f3b4fb1b379b1bd8dfa3a2c69
Robby Nelson <robby_nelson@lawn-128-61-32-57.lawn.gatech.edu>
2020-09-04 01:56:59 -0400

    Added comments, took away graphing. Trying to get agents to run faster...

--- SUSD_3AGENT_MOVINGSOURCE.m
+++ SUSD_3AGENT_MOVINGSOURCE.m
@@ -3,6 +3,8 @@
 % Georgia Tech Systems Research Lab
 % rnelson71@gatech.edu
 
+% Trying to get agents to move faster
+
 %% Setup and Initialization
 clc;
 clear;
@@ -12,83 +14,87 @@ N = 4; % generate 4 agents, one for the source and 3 for the following
 initial_poses = [0.6 0.7 0.9 0; 0.6 0.2 0.8 0; 1.0 1.0 1.0 0];
 
 r = Robotarium('NumberOfRobots', N, 'ShowFigure', true, 'InitialConditions', initial_poses);
-si_to_uni = create_si_to_uni_dynamics();
+si_to_uni = create_si_to_uni_dynamics(); % Reference to the function for unicycle dynamic creation
 x = r.get_poses();
 r.step();
 %% SUSD Algorithm
-tic
 q = [1; 0];
 source = [0 0];
 theta = pi/2;
 counter = 0; % loop counter
-main_counter = 0;
-while main_counter < 200
+graphed_infinity_points = 0;
+while graphed_infinity_points < 45 % Stops once the full infinity has been created
 %% Calculation of Covariance Matrix, n direction
     counter = counter + 1;
-    agent_source_position = x(1:2, 4)';
-    difference_vector = source - agent_source_position;
+    agent_source_position = x(1:2, 4)'; % position of the malicious agent 
 
-    if (mod(counter, 100) == 0)
-        main_counter = main_counter + 1;
-        theta = theta + (1 / 30);
-        source = [cos(theta) cos(theta) * sin(theta)];
-        if (mod(counter, 300) == 0)
-            plot(agent_source_position(1), agent_source_position(2), 'r.','MarkerSize',5,'MarkerFaceColor','r');
-        end
+    if (mod(counter, 100) == 0) % Every 100 loop cycles
+        graphed_infinity_points = graphed_infinity_points + 1;
+        theta = theta + (1 / 6.5);
+        source = [cos(theta) cos(theta) * sin(theta)]; % Computes the location of points on the infinity based on angle
+%         if (mod(counter, 300) == 0)
+%             plot(agent_source_position(1), agent_source_position(2), 'r.','MarkerSize',5,'MarkerFaceColor','r');
+%         end
     end
     
-    tau=2;dtau=0.01;
+    difference_vector = source - agent_source_position; % direction for the malicious agent to move in
     x = r.get_poses();
     
     % Virtual Field Calculation
     virtual_field_vals = zeros(1, N);
     k1= 1 / 1;
     virtual_field_vals_bounded = zeros(1, N);
-    for i = 1:N
+    for i = 1:N % Calculating the virtual quadratic field observed at each agent
         virtual_field_vals(1, i) = ((2.5 * x(1, i) - 2.5 * agent_source_position(1))^2 + (2.5 * x(2, i) - 2.5 * agent_source_position(2))^2);
-        virtual_field_vals_bounded(1, i) = k1 * virtual_field_vals(1, i) + .2;
+        virtual_field_vals_bounded(1, i) = k1 * virtual_field_vals(1, i) + .2; % Gives the value a base threshold for when tracking agents are close to malicious agent
     end
-    rotation = [0, 1; -1, 0];
+    rotation = [0, 1; -1, 0]; % 90 degree rotation matrix
     first = x(1:2, 1)';
     second = x(1:2, 2)';
     third = x(1:2, 3)';
     position = [first; second; third];
-    center = mean(position);
+    center = mean(position); % Finds the average position of the agents for use in covariance calculation
     distance_from_center = position - center(ones(size(position,1),1),:);
     C = distance_from_center' * distance_from_center;
+    tau=2; % Used for calculating covariance
+    dtau=0.01;
     
     for t=0:dtau:tau
-        q=q+(eye(2,2)-q*q')*C*q*dtau; 
+        q=q+(eye(2,2)-q*q')*C*q*dtau; % Algorithm for computing direction of maximum covariance
     end
-    n_dir = rotation * q;
+    n_dir = rotation * q; % Go in the direction perpendicular to the covariance
 
     
 %% Formation Calculation
     weight_bounded = ones(3,3) * 1.2;
-    rdot_formation = zeros(3,2);
+    rdot_formation = zeros(3,2); % Matrix to store agent's direction to maintain formation
     a = .5;
     b = .55;
-    desired_distance=[0 b a;b 0 a;a a 0];
+    desired_distance=[0 b a;b 0 a;a a 0]; % Desired distances to the other agents
     for i=1:3
-        for j=1:3
+        for j=1:3 % Calculate the difference in desired position with respect to each other agent
             weight=norm(position(i,:)-position(j,:))-desired_distance(i,j);
-            if weight>=0
+            if weight>=0 % Distance to the other agent is larger than desired
                 weight_bounded(i,j)=1-exp(-weight);
-            else
+            else % Too close to the other agent
                 weight_bounded(i,j)=-1+exp(weight);
             end
+            % Adding the how much the current agent will have
+            % to go to to each other agent. All directions will sum to
+            % create a general direction to maintain formation
             rdot_formation(i,:)=rdot_formation(i,:)+weight_bounded(i,j)*(position(j,:)-position(i,:));
         end
     end
     
 %% Calculations and Setting Velocities
     formation_gain = 4;
-    ref_r1 = .1 * virtual_field_vals(1,1) * n_dir + formation_gain * rdot_formation(1,:)';
-    ref_r2 = .1 * virtual_field_vals(1,2) * n_dir + formation_gain * rdot_formation(2,:)';
-    ref_r3 = .1 * virtual_field_vals(1,3) * n_dir + formation_gain * rdot_formation(3,:)';
-    ref_r4 = 2* difference_vector';
+    virtual_field_gain = .18;
+    ref_r1 = virtual_field_gain * virtual_field_vals(1,1) * n_dir + formation_gain * rdot_formation(1,:)';
+    ref_r2 = virtual_field_gain * virtual_field_vals(1,2) * n_dir + formation_gain * rdot_formation(2,:)';
+    ref_r3 = virtual_field_gain * virtual_field_vals(1,3) * n_dir + formation_gain * rdot_formation(3,:)';
+    ref_r4 = 6 * difference_vector';
     ref_all = [ref_r1, ref_r2, ref_r3, ref_r4];
-    ref_all = ref_all ./ 30;
+    ref_all = ref_all ./ 20;
     
     unicycle = si_to_uni(ref_all, x);
     r.set_velocities(1:N, unicycle);
